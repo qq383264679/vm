d85add5d31db0ef90bc16d64ffddc4fa62f5ce75
fengchao <fengchao@fengchao-PC>
2014-05-22 16:32:01 +0800

    第二次！

--- src/com/feng/Test.java
+++ src/com/feng/Test.java
@@ -4,26 +4,84 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.feng.dao.BaseDao;
+import com.feng.model.Order;
+import com.feng.model.Product;
+import com.feng.model.User;
 import com.feng.util.SpringUtil;
 
 
 public class Test {
 	String[] acx = {"applicationContext.xml", "dbContext.xml"};
 	ApplicationContext ctx;
+	SessionFactory sessionFactory;
 	//初始化srping
 	public void init() {
 		 ctx = new ClassPathXmlApplicationContext(acx);
 	}
 	//test sessionFactory
-	public void sessionFactoryTest() {
+	public  SessionFactory sessionFactoryTest() {
+		System.out.println("sssx");
 		SessionFactory sessionFactory = (SessionFactory) ctx.getBean("sessionFactory");
 		System.out.println(sessionFactory);
+		
+		return sessionFactory;
 	}
 	public static void main(String[] args) {
 		Test t = new Test();
 		t.init();
-		t.sessionFactoryTest();
-
+		t.productTest();
+	}
+	
+	public void userTest() {
+		BaseDao baseDao = (BaseDao) ctx.getBean("userDao");
+		User u = new User();
+		u.setUserName("feng");
+		u.setPassword("111");
+		baseDao.saveEntity(u);
+	}
+	public void productTest() {
+		BaseDao baseDao = (BaseDao) ctx.getBean("productDao");
+		Product p = new Product();
+		p.setDirector("张艺谋");
+		p.setLanguage("国语");
+		p.setName("红高粱");
+		p.setStarring("11111111111111111111111111111111111111111111");
+		p.setGenre("爱情");
+		p.setPoster("1.jpg");
+		p.setStoryAbstract("343443");
+		
+		baseDao.saveEntity(p);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public void saveUserAndOrderTest() {
+		User u = new User();
+		u.setUserName("feng");
+		u.setPassword("111");
+		
+		sessionFactory = sessionFactoryTest();
+		sessionFactory.openSession().save(u);
+		
+		Order order = new Order();
+		order.setUser(u);
+		sessionFactory.openSession().save(order);
+		
 	}
 
 }
--- src/com/feng/model/User.java
+++ src/com/feng/model/User.java
@@ -3,29 +3,14 @@ package com.feng.model;
 import java.util.Date;
 
 public class User {
-	private Integer id;
-	private String email;
-	private String name;
+	private String userName;
 	private String password;
-	private String nickName;
 	private Date regDate = new Date();  //×¢²áÊ±¼ä
-	public Integer getId() {
-		return id;
+	public String getUserName() {
+		return userName;
 	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 	public String getPassword() {
 		return password;
@@ -33,16 +18,11 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	public String getNickName() {
-		return nickName;
-	}
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
 	public Date getRegDate() {
 		return regDate;
 	}
 	public void setRegDate(Date regDate) {
 		this.regDate = regDate;
 	}
+
 }
